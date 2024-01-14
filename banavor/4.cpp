faf11c9ac10385130cbcede620ab4bc7a253373a
Gor <gor.ga05@mail.ru>
2024-01-14 09:40:51 +0400

    banavori 4-i aveli aragy

--- banavor/4.cpp
+++ banavor/4.cpp
@@ -1,46 +1,32 @@
-//18 ej 73
-
 #include <iostream>
 using namespace std;
 
+void igid(int arr[], int count)
+{
+   int arr1[count];
+   int counter = 0;
+
+
+   for (int i = 0; i < count - 1; i++)
+   {
+      int minIndex = i;
+      int counter1 = 0;
+      for (int j = i + 1; j < count; j++)
+         if (arr[minIndex] < arr[j])
+            counter1++;
+
+      
+      if (counter1 > counter)
+         counter = counter1;
+   }
+
+   cout << counter;
+}
+
 int main()
 {
-    const int n = 6;
-    int arr[n];
-    int arr1[n];
+   const int n = 6;
+   int arr[n]{1, 5, 6, 1, 0, 6};
 
-
-    for(int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-for(int t = 0; t < n; t++)
-{
-        int counter = 0;
-
-    for(int i = t; i < n; i++)
-    {
-        if ( arr[i] < arr [i+1])
-        {
-            counter++;
-        }
-
-    }
-
-     arr1[t] = counter;
-
-}
-            int r = arr1[0];
-
-    for(int t = 1; t < n; t++)
-    {
-        if( r  <  arr1[t])
-        {
-            r = arr1[t];
-        }
-    }
-    
-        cout << r;
-    return 0;
+   igid(arr, n);
 }

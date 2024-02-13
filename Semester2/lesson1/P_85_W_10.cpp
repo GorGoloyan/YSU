899521d741fbb0393399333f40c7de3ec8d846ba
Gor <gor.ga05@mail.ru>
2024-02-13 13:03:43 +0400

    +10

--- Semester2/lesson1/P_85_W_10.cpp
+++ Semester2/lesson1/P_85_W_10.cpp
@@ -1,14 +1,16 @@
-//?
 #include <iostream>
 using namespace std;
 
 bool isParz(int a)
 {
-    if (a == 1)
-        return 1;
-    if (a < 1)
+
+    if (a <= 1)
         return 0;
-    for (int i = 2; i < a / 2; i++)
+    if (a == 2)
+        return 1;
+    if (a % 2 == 0)
+        return 0;
+    for (int i = 3; i * i <= a; i += 2)
     {
         if (a % i == 0)
             return 0;
@@ -22,13 +24,19 @@ int main()
     int count = 0, i;
     int arr[size]{2, 5, 7, 8, -1, 6, 7, 10};
 
+    bool t = 0;
     for (int i = 0; i < size; i++)
     {
+
         if (arr[i] < 0)
+        {
+            t = 1;
             break;
+        }
+
         else if (isParz(arr[i]))
             count++;
     }
 
-    cout << ((arr[i] < 0) ? count : -1);
+    cout << ((t) ? count : -1);
 }

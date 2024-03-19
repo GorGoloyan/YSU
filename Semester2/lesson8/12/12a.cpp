71adc4d7f42326ba039ea921958eed279c940ca1
Gor <gor.ga05@mail.ru>
2024-03-19 15:18:18 +0400

    losson8

--- Semester2/lesson8/12/12a.cpp
+++ Semester2/lesson8/12/12a.cpp
@@ -5,32 +5,32 @@ using namespace std;
 int main()
 {
 
-                                                                                                    char tox[150];
-                                                                                                    cin.getline(tox, 150, '.');
-                                                                                                    int count = 0;
-                                                                                                    // bareri qanaky
-                                                                                                    for (int i = 0; i < strlen(tox);)
-                                                                                                    {
-                                                                                                                                                                                                        count++;
-                                                                                                                                                                                                        while (tox[i] == ' ')
-                                                                                                                                                                                                                                                                                                            i++;
+    char tox[150];
+    cin.getline(tox, 150, '.');
+    int count = 0;
+    // bareri qanaky
+    for (int i = 0; i < strlen(tox);)
+    {
+        count++;
+        while (tox[i] == ' ')
+            i++;
 
-                                                                                                                                                                                                        while (tox[i] != ' ')
-                                                                                                                                                                                                        {
-                                                                                                                                                                                                                                                                                                            i++;
-                                                                                                                                                                                                        }
-                                                                                                    }
-                                                                                                    // nax qanaky
-                                                                                                    int nax = 0;
-                                                                                                    // toxeriqanak
-                                                                                                    int toxeriqanak = 1;
-                                                                                                    for (int i = 0; i < strlen(tox); i++)
-                                                                                                                                                                                                        if (tox[i] == ':')
-                                                                                                                                                                                                                                                                                                            nax++;
-                                                                                                                                                                                                        else if (tox[i] == '\n')
-                                                                                                                                                                                                                                                                                                            toxeriqanak++;
+        while (tox[i] != ' ')
+        {
+            i++;
+        }
+    }
+    // nax qanaky
+    int nax = 0;
+    // toxeriqanak
+    int toxeriqanak = 1;
+    for (int i = 0; i < strlen(tox); i++)
+        if (tox[i] == ':')
+            nax++;
+        else if (tox[i] == '\n')
+            toxeriqanak++;
 
-                                                                                                    cout << toxeriqanak << '\t' << nax << '\t' << count;
+    cout << toxeriqanak << '\t' << nax << '\t' << count;
 
-                                                                                                    return 0;
+    return 0;
 }
